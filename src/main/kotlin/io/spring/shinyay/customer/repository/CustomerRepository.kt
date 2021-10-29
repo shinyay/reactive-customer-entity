fc8c0fee7e5ff59b4b89a677a25181251c8392ab
shinyay <shinya.com@gmail.com>
2021-10-29 11:29:52 +0900

    Add a ReactiveCrudRepository

--- src/main/kotlin/io/spring/shinyay/customer/repository/CustomerRepository.kt
+++ src/main/kotlin/io/spring/shinyay/customer/repository/CustomerRepository.kt
@@ -1,4 +1,6 @@
 package io.spring.shinyay.customer.repository
 
-class CustomerRepository {
-}
+import io.spring.shinyay.customer.entity.Customer
+import org.springframework.data.repository.reactive.ReactiveCrudRepository
+
+interface CustomerRepository : ReactiveCrudRepository<Customer, Long>
