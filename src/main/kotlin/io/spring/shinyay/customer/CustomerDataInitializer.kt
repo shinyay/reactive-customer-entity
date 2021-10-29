ffd754494b699689183e20704a50bb1051e91523
shinyay <shinya.com@gmail.com>
2021-10-29 12:06:38 +0900

    Add ApplicationRunner class to initilaize data

---
+++ src/main/kotlin/io/spring/shinyay/customer/CustomerDataInitializer.kt
@@ -0,0 +1,17 @@
+package io.spring.shinyay.customer
+
+import io.spring.shinyay.customer.entity.Customer
+import io.spring.shinyay.customer.repository.CustomerRepository
+import org.springframework.boot.ApplicationArguments
+import org.springframework.boot.ApplicationRunner
+import org.springframework.stereotype.Component
+import reactor.core.publisher.Flux
+
+@Component
+class CustomerDataInitializer(val repository: CustomerRepository) : ApplicationRunner {
+
+    override fun run(args: ApplicationArguments?) {
+        val names = Flux.just("John", "Jack", "Mary", "Jane", "Jill", "Joe")
+        val customers = names.map { name -> Customer(0, name) }
+    }
+}
